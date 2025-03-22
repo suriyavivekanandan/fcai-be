1f6b52294d789d1f176d03fcc811568ae01859b0
suriyavivekanandan <130919330+suriyavivekanandan@users.noreply.github.com>
2025-03-22 20:15:59 +0530

    Update server.js
    for render deployment

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const app = express()
 const server = http.createServer(app)
 
 app.use(
-    cors({
-        origin: [config.API_HOST, config.CORS_ORIGIN],  
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    }),
+  cors({
+    origin: [config.API_HOST, config.CORS_ORIGIN],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  })
 )
 
 app.use(json({ limit: '16kb' }))
@@ -36,21 +36,26 @@ console.log(`\x1b[95mSERVER IP: ${serverIP}`)
 app.get('/', (req, res) => res.json({ status: 'UP', message: 'Server runs' }))
 app.use('/api', router)
 
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).json({ status: 'ERROR', message: 'Something went wrong!' })
 })
 
+// Connect to DB and start server
 connectDatabase((isConnect) => {
-    if (isConnect) {
-        server.listen(config.PORT, () => {
-            console.log(`\x1b[33mServer runs in port ${config.PORT}...`)
-            console.log(`\x1b[38;5;201mAPI HOST - http://${serverIP}:${config.PORT} or http://127.0.0.1:${config.PORT} or ${config.API_HOST} \n`)
-        })
-    } else {
-        console.error('Failed to connect to the database. Server not started.')
-        process.exit(1)
-    }
+  if (isConnect) {
+    // ✅ BIND TO 0.0.0.0 for Render
+    server.listen(config.PORT, '0.0.0.0', () => {
+      console.log(`\x1b[33mServer runs on port ${config.PORT}...`)
+      console.log(
+        `\x1b[38;5;201mAPI HOST - http://${serverIP}:${config.PORT} or http://127.0.0.1:${config.PORT} or ${config.API_HOST} \n`
+      )
+    })
+  } else {
+    console.error('Failed to connect to the database. Server not started.')
+    process.exit(1)
+  }
 })
 
 export { server }
